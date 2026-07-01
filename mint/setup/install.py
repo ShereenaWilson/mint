31bd9358ae44c29a6d2b2bd09e41bafa2e7b687f
ShereenaWilson <158040625+ShereenaWilson@users.noreply.github.com>
2026-07-01 09:23:17 +0530

    fix: skip is_credit_card and reconciliation_type if already standard fields in ERPNext v16
    Added a custom field 'is_credit_card' to 'Bank Account' if it doesn't already exist.

--- mint/setup/install.py
+++ mint/setup/install.py
@@ -4,15 +4,6 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def after_install():
 		custom_fields = {
-					"Bank Account": [
-									{
-														"fieldname": "is_credit_card",
-														"fieldtype": "Check",
-														"label": "Is Credit Card",
-														"insert_after": "bank_account_no",
-														"default": 0,
-									}
-					],
 					"Bank Transaction": [
 									{
 														"fieldname": "is_rule_evaluated",
@@ -34,7 +25,22 @@ def after_install():
 					],
 		}
 
-	# Only add reconciliation_type if it is not already a standard field in the DocType
+	# Only add is_credit_card if it is not already a standard field in Bank Account
+		bank_account_fieldnames = [
+			f.fieldname for f in frappe.get_meta("Bank Account").fields
+		]
+		if "is_credit_card" not in bank_account_fieldnames:
+					custom_fields["Bank Account"] = [
+									{
+														"fieldname": "is_credit_card",
+														"fieldtype": "Check",
+														"label": "Is Credit Card",
+														"insert_after": "bank_account_no",
+														"default": 0,
+									}
+					]
+
+		# Only add reconciliation_type if it is not already a standard field in Bank Transaction Payments
 		existing_fieldnames = [
 			f.fieldname for f in frappe.get_meta("Bank Transaction Payments").fields
 		]
@@ -46,8 +52,6 @@ def after_install():
 														"label": "Reconciliation Type",
 														"options": "Matched\nVoucher Created",
 														"insert_after": "clearance_date",
-														"read_only": 1,
-														"default": "Matched",
 									}
 					]
 
